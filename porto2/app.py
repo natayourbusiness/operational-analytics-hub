e50c83ce97aa961638a2664e16e566f90d45bb08
natayourbusiness <donnypranata45@gmail.com>
2026-06-29 08:46:36 +0700

    Update app.py

--- porto2/app.py
+++ porto2/app.py
@@ -18,21 +18,21 @@ Sebagai seorang "Data Analyst and Operations Professional" dengan fondasi kuat d
 
 Aplikasi ini adalah bukti langsung bagaimana "raw operational datasets" dapat diekstraksi dan ditransformasi menjadi "strategic insights" yang dapat ditindaklanjuti oleh manajemen untuk menghentikan inefisiensi.  
 
-🛠️ **Core Architecture:***
-Dibangun menggunakan "Python, Streamlit, and SQL" dengan implementasi pemodelan "Relational Database Architecture" dan algoritma "K-Means clustering".  
+🛠️ **Core Architecture:**
+Dibangun menggunakan *"Python, Streamlit, and SQL"* dengan implementasi pemodelan *"Relational Database Architecture"* dan algoritma *"K-Means clustering".*  
 
 📊 **Modul Operasional (Proof of Concept):**
 
 Jangan hanya membaca CV saya. Uji langsung sistem yang telah saya rancang dengan memilih modul di bawah ini:
 
 📦 **Inventory Health & Expiry Audit**
-Sistem rantai pasok prediktif yang secara otomatis menghitung "days-to-expiry and reorder thresholds" untuk mencegah *"dead-stock financial loss".*  
+    Sistem rantai pasok prediktif yang secara otomatis menghitung "days-to-expiry and reorder thresholds" untuk mencegah *"dead-stock financial loss".*  
 
 🏭 **Manufacturing Defect & Cost Tracker**
-Alat "root-cause analysis" yang melacak anomali produksi, mengkuantifikasi *"financial leakage"*, dan meresepkan intervensi *"Quality Control (QC)"* secara presisi.  
+    Alat "root-cause analysis" yang melacak anomali produksi, mengkuantifikasi *"financial leakage"*, dan meresepkan intervensi *"Quality Control (QC)"* secara presisi.  
 
 💼 **Advanced Sales & Revenue Audit**
-Mesin audit dinamis yang mengintegrasikan *"Pareto (80/20) analysis"* untuk mengidentifikasi *"top-performing product lines"* dan memetakan nilai pelanggan.  
+    Mesin audit dinamis yang mengintegrasikan *"Pareto (80/20) analysis"* untuk mengidentifikasi *"top-performing product lines"* dan memetakan nilai pelanggan.  
 
 👈 **Buka navigasi di Sidebar Kiri untuk memulai simulasi sistem.**
 
